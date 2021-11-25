15444820a6dd6fe3031191602a42173f4e285de0
parthpanse <parthpanse050401@gmail.com>
2021-11-25 09:51:45 +0530

    CSS added

--- admin_login.php
+++ admin_login.php
@@ -3,18 +3,30 @@
 <head>
 	<title>Admin Login</title>
 	<link rel="stylesheet" type="text/css" href="bootstrap-4.4.1/css/bootstrap.min.css">
-  	<script type="text/javascript" src="bootstrap-4.4.1/js/juqery_latest.js"></script>
-  	<script type="text/javascript" src="bootstrap-4.4.1/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="bootstrap-4.4.1/js/juqery_latest.js"></script>
+	<script type="text/javascript" src="bootstrap-4.4.1/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<script src="https://use.fontawesome.com/releases/v5.13.1/js/all.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="css/admin_login.css">
 </head>
 <body>
+
+	<div class="header">
+
+		<a href="login.php"><img class="home_button" src="images/house.png"></a>
+		<center>
+			<h1>Institute of Information Technology</h1>
+		</center>
+	</div>
 	<center><br><br>
-		<h3>Admin LogIn Page</h3><br>
+		<h3>Admin Login Page</h3><br>
 		<form action="" method="post">
 			Email ID: <input type="text" name="email" required><br><br>
 			Password: <input type="password" name="password" required><br><br>
-			<input type="submit" name="submit" value="LogIn">
+			<input class="btn btn-primary" type="submit" name="submit" value="LogIn">
 		</form><br>
-		
+
 		<?php
 			session_start();
 			if(isset($_POST['submit'])){
@@ -37,7 +49,7 @@
 						echo "Wrong Email";
 					}
 				}
-				
+
 			}
 		?>
 	</center>
